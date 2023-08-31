73e6b8e1c2964cab24f6270b46066427f43e2332
clint <1127524303@qq.com>
2023-08-31 21:32:39 +0800

    8-24

--- konwledge/pointer/function_pointer.c
+++ konwledge/pointer/function_pointer.c
@@ -1,8 +1,22 @@
 /*函数指针*/
 #include <stdio.h>
+int add(int x,int y)
+{
+    printf("%d,%d\n",x,y);
+    return (x+y);
+}
 
 int main(int argc, char const *argv[])
 {
     /* code */
+    int (*f)();
+    f = add;
+    //调用方式1
+    add(2,3);
+    //调用方式2
+    f(4,5);
+    //调用方式3
+    (*f)(7,9);
+
     return 0;
 }
