08222d7485e0432cd629965e1ae310705afd6ead
clint <1127524303@qq.com>
2023-09-06 23:18:44 +0800

    pointer.c update

---
+++ konwledge/pointer/pointer.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    void swap(int *p1,int *p2);
+    int a,b;
+    int *pointer_1,*pointer_2;
+    printf("please enter a and b: \n");
+    scanf("%d,%d",&a,&b);
+    pointer_1 = &a;
+    pointer_2 = &b;
+    if(a<b) swap1(pointer_1,pointer_2);
+    printf("max = %d, min = %d \n",a,b); 
+    return 0;
+}
+
+void swap(int *p1, int *p2)
+{
+    int temp = *p1; 
+    *p1 = *p2;
+    *p2 = temp;
+}
+
+void swap1(int x,int y)
+{
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
